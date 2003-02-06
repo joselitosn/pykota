0eca95a2b5d94f176535e7875e711b9be8b1862e
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2003-02-06 09:19:02 +0000

    More robust behavior (hopefully) when the user or printer is not managed correctly by the Quota System : e.g. cupsFilter added in ppd file, but printer and/or user not 'yet?' in storage.

--- pykota/trunk/pykota/config.py
+++ pykota/trunk/pykota/config.py
@@ -14,6 +14,11 @@
 # $Id$
 #
 # $Log$
+# Revision 1.4  2003/02/06 09:19:02  jalet
+# More robust behavior (hopefully) when the user or printer is not managed
+# correctly by the Quota System : e.g. cupsFilter added in ppd file, but
+# printer and/or user not 'yet?' in storage.
+#
 # Revision 1.3  2003/02/05 23:26:22  jalet
 # Incorrect handling of grace delay
 #
@@ -76,8 +81,8 @@ class PyKotaConfig :
                 if not self.config.has_option(printer, poption) :
                     raise PyKotaConfigError, "Option %s not found in section %s of %s" % (option, printer, self.filename)
                     
-            validpolicies = [ "accept", "deny" ]     
-            if self.config.get(printer, "policy").lower() not in validpolicies :
+            validpolicies = [ "ALLOW", "DENY" ]     
+            if self.config.get(printer, "policy").upper() not in validpolicies :
                 raise PyKotaConfigError, "Option policy in section %s only supports values in %s" % (printer, str(validrequesters))
             
             validrequesters = [ "snmp" ] # TODO : add more requesters
@@ -118,7 +123,7 @@ class PyKotaConfig :
         
     def getPrinterPolicy(self, printer) :    
         """Returns the default policy for the current printer."""
-        return self.config.get(printer, "policy").lower()
+        return self.config.get(printer, "policy").upper()
         
     def getSMTPServer(self) :    
         """Returns the SMTP server to use to send messages to users."""
