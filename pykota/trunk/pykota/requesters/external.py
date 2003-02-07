dd2bef8ba399f4f885713cea27c2d8b93f75dcbd
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2003-02-07 13:15:01 +0000

    External requester skeleton added.

---
+++ pykota/trunk/pykota/requesters/external.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+# PyKota - Print Quotas for CUPS
+#
+# (c) 2003 Jerome Alet <alet@librelogiciel.com>
+# You're welcome to redistribute this software under the
+# terms of the GNU General Public Licence version 2.0
+# or, at your option, any higher version.
+#
+# You can read the complete GNU GPL in the file COPYING
+# which should come along with this software, or visit
+# the Free Software Foundation's WEB site http://www.fsf.org
+#
+# $Id$
+#
+# $Log$
+# Revision 1.1  2003/02/07 13:15:01  jalet
+# External requester skeleton added.
+#
+#
+#
+
+import os
+from pykota.requester import PyKotaRequesterError
+
+class Requester :
+    """A class to send queries to printers via external commands."""
+    def __init__(self, config, printername) :
+        """Sets instance vars depending on the current printer."""
+        self.printername = printername
+        raise PyKotaRequesterError, "Requester not implemented yet."
+        
+    def getPrinterPageCounter(self, hostname) :
+        """Returns the page counter from the hostname printer via SNMP.
+        
+           Currently uses the snmpget external command. TODO : do it internally 
+        """
+        raise PyKotaRequesterError, "Requester not implemented yet."
+        
