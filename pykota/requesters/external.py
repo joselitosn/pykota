43f679d8585ff15bd1e5017e163f29b5ee9ea3f4
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2003-02-10 00:42:17 +0000

    External requester should be ok (untested) New syntax for configuration file wrt requesters

--- pykota/requesters/external.py
+++ pykota/requesters/external.py
@@ -14,6 +14,10 @@
 # $Id$
 #
 # $Log$
+# Revision 1.3  2003/02/10 00:42:17  jalet
+# External requester should be ok (untested)
+# New syntax for configuration file wrt requesters
+#
 # Revision 1.2  2003/02/09 13:05:43  jalet
 # Internationalization continues...
 #
@@ -28,15 +32,24 @@ from pykota.requester import PyKotaRequesterError
 
 class Requester :
     """A class to send queries to printers via external commands."""
-    def __init__(self, config, printername) :
+    def __init__(self, config, printername, arguments) :
         """Sets instance vars depending on the current printer."""
         self.printername = printername
-        raise PyKotaRequesterError, _("Requester not implemented yet.")
+        self.commandline = arguments[0]
         
-    def getPrinterPageCounter(self, hostname) :
-        """Returns the page counter from the hostname printer via SNMP.
+    def getPrinterPageCounter(self, printer) :
+        """Returns the page counter from the printer via an external command.
         
-           Currently uses the snmpget external command. TODO : do it internally 
+           The external command must report the life time page number of the printer on stdout.
         """
-        raise PyKotaRequesterError, _("Requester not implemented yet.")
+        commandline = self.commandline % locals()
+        if printer is None :
+            raise PyKotaRequesterError, _("Unknown printer address in EXTERNAL(%s) for printer %s") % (commandline, self.printername)
+        answer = os.popen(commandline)
+        try :
+            pagecounter = int(answer.readline().strip())
+        except IndexError :    
+            raise PyKotaRequesterError, _("Unable to query printer %s via EXTERNAL(%s)") % (printer, commandline) 
+        answer.close()
+        return pagecounter
         
