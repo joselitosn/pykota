75a344c28906e9b0207691aefef2c4bac81a80aa
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2003-03-26 17:48:36 +0000

    First shot at trying to detect the availability of the needed software during the installation.

--- pykota/trunk/setup.py
+++ pykota/trunk/setup.py
@@ -16,6 +16,10 @@
 # $Id$
 #
 # $Log$
+# Revision 1.3  2003/03/26 17:48:36  jalet
+# First shot at trying to detect the availability of the needed software
+# during the installation.
+#
 # Revision 1.2  2003/03/09 16:49:04  jalet
 # The installation script installs the man pages too now.
 #
@@ -33,6 +37,26 @@ from distutils.core import setup
 sys.path.insert(0, "pykota")
 from pykota.version import __version__, __doc__
 
+if "install" in sys.argv :
+    sys.stderr.write("PygreSQL availability : ")
+    try :
+        import pg
+    except ImportError :    
+        sys.stderr.write("No ! Installation aborted.\n")
+        sys.exit(-1)
+    else :    
+        del pg
+        sys.stderr.write("OK.\n")
+        sys.stderr.write("snmpget availability : ")
+        result = os.popen("type snmpget")
+        snmpget = result.read().strip()
+        result.close()
+        if not snmpget :
+            sys.stderr.write("No ! Installation aborted.\n")
+            sys.exit(-1)
+        else :    
+            sys.stderr.write("OK.\n")
+            
 data_files = []
 mofiles = glob.glob(os.sep.join(["po", "*", "*.mo"]))
 for mofile in mofiles :
