1176d528037c4fd9e5cdb81281d8aefb7749b6be
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2003-04-15 17:39:57 +0000

    Support for AppleTalk requester was added and tested successfully with an Apple LaserWriter 16/600 PS and an HP 2100 TN with Ethertalk enabled.

--- untested/netatalk/netatalk.sh
+++ untested/netatalk/netatalk.sh
@@ -2,9 +2,14 @@
 #
 # $Id$
 #
-# The following was adapted from a post found on usenet, 
-# it works with my Apple LaserWriter 16/600 PS.
+# The following was adapted from a post found on usenet.
+#
+# It works with my Apple LaserWriter 16/600 PS and with
+# my HP LasetJet 2100 TN with AppleTalk enabled.
+#
 # As always, YMMV.
 # 
-/usr/bin/pap -p "MyPrinter:LaserWriter" pagecount.ps 2>/dev/null | grep -v status | grep -v Connect
-
+echo Please uncomment one of the lines, adapt the script and restart.
+echo You can get AppleTalk printer\'s names with the nbplkup command.
+# /usr/bin/pap -p "RAMPAL-16/600:LaserWriter" pagecount.ps 2>/dev/null | grep -v status | grep -v Connect
+# /usr/bin/pap -p "LaserJet 2100 NT:LaserWriter" pagecount.ps 2>/dev/null | grep -v status | grep -v Connect
