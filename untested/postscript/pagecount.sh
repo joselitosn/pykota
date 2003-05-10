1d64961013802086f342f668c6da9bd639845cfc
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2003-05-10 06:37:58 +0000

    Small simplification so that it reads the job's data from standard input. This way it is now directly useable as a PyKota external accounter.

--- untested/postscript/pagecount.sh
+++ untested/postscript/pagecount.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
 #
-# Adapted and modified from the printquota project 
-# http://printquota.sourceforge.net
+# PyKota
+#
+# PyKota : Print Quotas for CUPS and LPRng
+#
+# (c) 2003 Jerome Alet <alet@librelogiciel.com>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
+#
+#
+#
+# This script was freely adapted and modified from the printquota project 
+# http://printquota.sourceforge.net for inclusion into PyKota.
 #
 # printquota (c) 2002-2003 Ahmet Ozturk & Cagatay Koksoy
 # printquota is distributed under the GNU General Public License
 #
-# Usage : pagecount.sh file.ps
+# Usage : pagecount.sh <file.ps
 #
 (
-(cat $* | sed -n -e '/%!PS-Adobe-/,/\%\%EOF/p' | grep -E -v "/Duplex|PageSize")
+(sed -n -e '/%!PS-Adobe-/,/\%\%EOF/p' | grep -E -v "/Duplex|PageSize")
 echo currentdevice /PageCount gsgetdeviceprop == flush
 ) | gs -q -sDEVICE=bit -sOutputFile=/dev/null -r5 - | tail -1
 
