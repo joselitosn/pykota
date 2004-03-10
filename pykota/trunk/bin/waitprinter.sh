eba09e4e323b31087535953022702dff10003797
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2004-03-10 07:20:03 +0000

    More powerful waitprinter.sh

--- pykota/trunk/bin/waitprinter.sh
+++ pykota/trunk/bin/waitprinter.sh
@@ -13,10 +13,24 @@
 #
 # $Id$
 #
-# Fix by Matt Hyclak :
-sleep 5
-#
+# Fix by Matt Hyclak & Jerome Alet
+
+# If ending phase, after the job has been fully transmitted to the printer
+# we have to wait for the printer being in printing mode before checking
+# if it is idle, otherwise we could have problems with slow printers.
+# When using the pykota filter, PYKOTAPHASE is not defined at the
+# first requesting stage (which is the ending phase of the previous
+# job), because waiting for the printer being printing wouldn't be OK :
+# the printer would effectively never print again if the previous job
+# was already fully printed.
+if [ x$PYKOTAPHASE == "xAFTER" ]; then
+  until snmpget -v1 -c public -Ov $1 HOST-RESOURCES-MIB::hrPrinterStatus.1 | grep -i printing >/dev/null; do
+   sleep 1 ;
+  done
+fi
+
+# In any case, wait until the printer is idle again.
 until snmpget -v1 -c public -Ov $1 HOST-RESOURCES-MIB::hrPrinterStatus.1 | grep -i idle >/dev/null ; do 
-   sleep 1 ; 
+  sleep 1 ;
 done
 
