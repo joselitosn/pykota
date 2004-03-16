b6f74b9f222b443d7782b36628a6513d69aad179
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2004-03-16 12:05:02 +0000

    Small fix for new waitprinter.sh : when job was denied, would wait forever for printer being in printing mode.

--- bin/waitprinter.sh
+++ bin/waitprinter.sh
@@ -23,7 +23,7 @@
 # job), because waiting for the printer being printing wouldn't be OK :
 # the printer would effectively never print again if the previous job
 # was already fully printed.
-if [ x$PYKOTAPHASE == "xAFTER" ]; then
+if [ x$PYKOTAACTION != "xDENY" ] && [ x$PYKOTAPHASE == "xAFTER" ]; then
   until snmpget -v1 -c public -Ov $1 HOST-RESOURCES-MIB::hrPrinterStatus.1 | grep -i printing >/dev/null; do
    sleep 1 ;
   done
