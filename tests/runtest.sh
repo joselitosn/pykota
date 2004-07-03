a638245e94a4f9c96c38df2710024db280d8c1c7
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2004-07-03 08:29:26 +0000

    Replaced uncompressed version of the PostScript file with a compressed version

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 
 echo -n "Generating testsuite..."
-/bin/rm -f testsuite.*
+/bin/rm -f testsuite.* master2.ps
+gunzip <master.ps.gz >master2.ps
 for device in laserjet ljet3 ljet4 lj5gray lj5mono pxlmono pxlcolor pdfwrite pswrite psgray psmono psrgb ; do
-    gs -dQUIET -dBATCH -dNOPAUSE -sOutputFile="testsuite.$device" -sDEVICE="$device" master.ps ;
+    gs -dQUIET -dBATCH -dNOPAUSE -sOutputFile="testsuite.$device" -sDEVICE="$device" master2.ps ;
     done
 echo 
 
 echo -n "File master.ps should be 16 pages long, result is : "
-python ../pykota/pdlanalyzer.py master.ps
+python ../pykota/pdlanalyzer.py master2.ps
 
 echo "Analyzing testsuite..."
 for file in testsuite.* ; do
