4fd0b36fe9f780ad74ec9b23d48660e5453401d7
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2004-07-21 13:21:52 +0000

    Added script to extract messages

---
+++ extractmsg.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+#
+# PyKota - Print Quotas for CUPS and LPRng
+#
+# (c) 2003-2004 Jerome Alet <alet@librelogiciel.com>
+# You're welcome to redistribute this software under the
+# terms of the GNU General Public Licence version 2.0
+# or, at your option, any higher version.
+#
+# You can read the complete GNU GPL in the file COPYING
+# which should come along with this software, or visit
+# the Free Software Foundation's WEB site http://www.fsf.org
+#
+# $Id$
+#
+rm -f /tmp/pykota.po
+xgettext --language=python --default-domain=pykota --output-dir=/tmp --no-location --msgid-bugs-address="alet@librelogiciel.com" $*
+
