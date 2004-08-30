751578ad355f3517efec370f171aadd6f3f5f555
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2004-08-30 23:10:25 +0000

    Improved the ESC/P2 analyzer so that more GhostScript devices are supported

--- pykota/trunk/pykota/version.py
+++ pykota/trunk/pykota/version.py
@@ -21,7 +21,7 @@
 # $Id$
 #
 
-__version__ = "1.20alpha3_unofficial"
+__version__ = "1.20alpha4_unofficial"
 
 __doc__ = """PyKota : a complete Printing Quota Solution for CUPS and LPRng."""
 
--- pykota/trunk/tests/runtest.sh
+++ pykota/trunk/tests/runtest.sh
@@ -3,7 +3,7 @@
 echo -n "Generating testsuite..."
 /bin/rm -f testsuite.* master2.ps
 gunzip <master.ps.gz >master2.ps
-for device in laserjet ljet3 ljet4 lj5gray lj5mono pxlmono pxlcolor pdfwrite pswrite psgray psmono psrgb epson epsonc eps9mid eps9high ; do
+for device in laserjet ljet3 ljet4 lj5gray lj5mono pxlmono pxlcolor pdfwrite pswrite psgray psmono psrgb epson epsonc eps9mid eps9high stcolor st800 escp escpc ; do
     gs -dQUIET -dBATCH -dNOPAUSE -sOutputFile="testsuite.$device" -sDEVICE="$device" master2.ps ;
     done
 echo 
