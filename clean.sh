5825c8535e677cecdce23eabe4ce304742ba9743
jalet <jalet@6164e503-883a-4105-99cd-1bc75b88fced>
2005-03-05 19:47:21 +0000

    Now deletes Debian temporary directory

--- clean.sh
+++ clean.sh
@@ -31,4 +31,5 @@ find . -name "*.jem" -exec rm -f {} \;
 find docs -name "*.html" -exec rm -f {} \;
 find docs -name "*.pdf" -exec rm -f {} \;
 find docs -name "*.tex" -exec rm -f {} \;
+rm -fr debian/tmp/
 rm -fr docs/pykota/ docs/pykota.junk/
