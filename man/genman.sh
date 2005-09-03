7ad585af62f359fdeeaa59dea0959562a0d7873a
Jérôme Alet <85658105+tamere-allo-peter@users.noreply.github.com>
2005-09-03 13:48:56 +0000

    Added the --dryrun command line option to pkturnkey. Improved pkturnkey's general usability. Added pkturnkey's manual page. Updated other manpages. Severity : minor.

--- man/genman.sh
+++ man/genman.sh
@@ -13,7 +13,7 @@
 #
 # $Id$
 #
-for prog in pkbcodes pkmail pkbanner autopykota dumpykota edpykota pykotme repykota warnpykota pkprinters pkhint pykosd ; do 
+for prog in pkturnkey pkbcodes pkmail pkbanner autopykota dumpykota edpykota pykotme repykota warnpykota pkprinters pkhint pykosd ; do 
     echo $prog ;
     help2man --no-info --section=1 --manual "User Commands" --source="C@LL - Conseil Internet & Logiciels Libres" --output=$prog.1 $prog ; 
     cd ../po ;
