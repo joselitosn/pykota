20cd94905ed7c4cb89f1f3e620687c8b6237552b
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2006-02-03 14:14:59 +0000

    Added svn keywords (Id)

--- pykota/trunk/pykota/storages/mysqlstorage.py
+++ pykota/trunk/pykota/storages/mysqlstorage.py
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-# $Id: pgstorage.py 1725 2005-09-03 12:20:30Z jerome $
+# $Id$
 #
 #
 
