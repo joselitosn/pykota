a64512f29089dc0c1859678a518b302ec9298e3e
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2006-03-28 22:03:44 +0000

    Improved documentation for PostgreSQL and MySQL backends setup.

--- pykota/trunk/initscripts/postgresql/pykota-postgresql.sql
+++ pykota/trunk/initscripts/postgresql/pykota-postgresql.sql
@@ -34,9 +34,10 @@ CREATE DATABASE pykota;
 
 --
 -- Create the print quota database users
+-- NOTE: Change the password values to the passwords you would like.
 -- 
-CREATE USER pykotaadmin;
-CREATE USER pykotauser;
+CREATE USER pykotauser WITH UNENCRYPTED PASSWORD 'readonlypw' NOCREATEDB NOCREATEUSER;
+CREATE USER pykotaadmin WITH UNENCRYPTED PASSWORD 'readwritepw' NOCREATEDB NOCREATEUSER;
 
 -- 
 -- Now connect to the new database
