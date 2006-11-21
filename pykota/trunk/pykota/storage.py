0b80cb587b8b23f567f819505ee60eb1e686b5d2
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2006-11-21 22:01:52 +0000

    Changed the order of usernames in default refunding reason.

--- pykota/trunk/pykota/storage.py
+++ pykota/trunk/pykota/storage.py
@@ -490,8 +490,8 @@ class StorageJob(StorageObject) :
         basereason = _("Refunded %i pages and %.3f credits by %s (%s) on %s") \
                         % (self.JobSize,
                            self.JobPrice,
-                           os.getlogin(),
                            self.parent.tool.originalUserName,
+                           os.getlogin(),
                            str(DateTime.now())[:19])
         if reason :                                               
             reason = "%s : %s" % (basereason, reason)
