dbc4ac153da9f2c83845ec9c5036cbe4ca6de8f8
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2007-11-29 09:29:01 +0000

    Ensure only developers will install the latest development tree for now, to prevent people from forming bad feelings about this (great) software.

--- pykota/trunk/setup.py
+++ pykota/trunk/setup.py
@@ -37,6 +37,10 @@ except ImportError, msg :
 sys.path.insert(0, "pykota")
 from pykota.version import __version__, __doc__
 
+if (__version__.upper().find("BROKEN") != -1) and ("--force" not in sys.argv[1:]) :
+    sys.stderr.write("THIS VERSION OF PYKOTA IS BROKEN. YOU MUST NOT USE IT. YOU SHOULD DOWNLOAD AN EARLIER RELEASE INSTEAD, EITHER THROUGH AN ENTRY PASS, OR BY DOWNLOADING THE LATEST STABLE TAG FROM SUBVERSION.\n")
+    sys.exit(-1)
+
 data_files = []
 mofiles = glob.glob(os.sep.join(["po", "*", "*.mo"]))
 for mofile in mofiles :
