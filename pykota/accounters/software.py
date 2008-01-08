abeec65bb68a904b1494b54c823c5800a2521923
Jérôme Alet <85658105+tamere-allo-peter@users.noreply.github.com>
2008-01-08 23:17:47 +0000

    Now external preaccounter/accounter scripts must read the print job's datas from the file pointed to by the PYKOTADATAFILE environment variable, instead of from their stdin.

--- pykota/accounters/software.py
+++ pykota/accounters/software.py
@@ -74,23 +74,10 @@ class Accounter(AccounterBase) :
     def withExternalScript(self) :    
         """Does software accounting through an external script."""
         self.filter.printInfo(_("Launching SOFTWARE(%s)...") % self.arguments)
-        MEGABYTE = 1024*1024
-        infile = open(self.filter.DataFile, "rb")
-        child = popen2.Popen4(self.arguments)
-        try :
-            data = infile.read(MEGABYTE)    
-            while data :
-                child.tochild.write(data)
-                data = infile.read(MEGABYTE)
-            child.tochild.flush()
-            child.tochild.close()    
-        except (IOError, OSError), msg :    
-            msg = "%s : %s" % (self.arguments, msg) 
-            self.filter.printInfo(_("Unable to compute job size with accounter %s") % msg)
-        infile.close()
         pagecounter = None
+        child = os.popen(self.arguments, "r")
         try :
-            answer = child.fromchild.read()
+            answer = child.read()
         except (IOError, OSError), msg :    
             msg = "%s : %s" % (self.arguments, msg) 
             self.filter.printInfo(_("Unable to compute job size with accounter %s") % msg)
@@ -103,16 +90,14 @@ class Accounter(AccounterBase) :
                     self.filter.printInfo(_("Line [%s] skipped in accounter's output. Trying again...") % lines[i])
                 else :    
                     break
-        child.fromchild.close()
-        
+                    
+        status = child.close()
         try :
-            status = child.wait()
-        except OSError, msg :    
-            self.filter.printInfo(_("Problem while waiting for software accounter pid %s to exit : %s") % (child.pid, msg))
-        else :    
             if os.WIFEXITED(status) :
                 status = os.WEXITSTATUS(status)
-            self.filter.printInfo(_("Software accounter %s exit code is %s") % (self.arguments, str(status)))
+        except TypeError :        
+            pass # None means no error occured.
+        self.filter.printInfo(_("Software accounter %s exit code is %s") % (self.arguments, str(status)))
             
         if pagecounter is None :    
             message = _("Unable to compute job size with accounter %s") % self.arguments
