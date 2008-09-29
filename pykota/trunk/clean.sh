9d3ffb4946b84b9b612bc2c63875d32b95164788
jerome <jerome@6164e503-883a-4105-99cd-1bc75b88fced>
2008-09-29 15:41:14 +0000

    Now cleans up emacs's temporary files too.

--- pykota/trunk/clean.sh
+++ pykota/trunk/clean.sh
@@ -24,6 +24,7 @@
 
 rm -f MANIFEST ChangeLog
 find . -name "*.bak" -exec rm -f {} \;
+find . -name "\#*\#" -exec rm -f {} \;
 find . -name "*~" -exec rm -f {} \;
 find . -name "*.pyc" -exec rm -f {} \;
 find . -name "*.pyo" -exec rm -f {} \;
