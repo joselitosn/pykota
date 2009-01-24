eeb7bf991dbb37af8a409f148c84a0455c3d8b77
Jérôme Alet <85658105+tamere-allo-peter@users.noreply.github.com>
2009-01-24 21:22:34 +0000

    Changed copyright years. Copyright years are now dynamic when displayed by a command line tool.

--- pykota/version.py
+++ pykota/version.py
@@ -2,7 +2,7 @@
 #
 # PyKota : Print Quotas for CUPS
 #
-# (c) 2003, 2004, 2005, 2006, 2007, 2008 Jerome Alet <alet@librelogiciel.com>
+# (c) 2003-2009 Jerome Alet <alet@librelogiciel.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -21,13 +21,15 @@
 
 """This module defines some application level constants."""
 
-__version__ = "1.27alpha6_unofficial"
+import time
+
+__version__ = "1.27alpha7_unofficial"
 
 __doc__ = "PyKota : a complete Printing Quota Solution for CUPS."
 
 __author__ = "Jerome Alet - alet@librelogiciel.com"
 
-__years__ = "2003-2008"
+__years__ = "2003-%s" % time.strftime("%Y", time.localtime(time.time()))
 
 __gplblurb__ = """This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
